8eeaeb9dbf70207851e00b069d8537efb531dafd
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-29 13:59:32 +0200

    initial commit

---
+++ array_2/main.js
@@ -0,0 +1,50 @@
+// const lista = ['word', 'excel', 'powerpoint', 'access'];
+
+// for (let i = 0; i < lista.length; i++){
+//     // console.log(lista[i]);
+
+//     let elemento = lista[i];
+//     // console.log(elemento);
+//     if (elemento == `excel`) {
+//         console.log(`Excel fa schifo`)
+//     } else {
+//         console.log(elemento)
+//     }
+// }
+
+
+
+
+// const sport = [`calcio`, `basket`, `tennis`, `pallavvolo`]
+// console.log(sport)
+
+// for(let i = 0; i < sport.length; i++) {
+//     console.log(sport[i]);
+// }
+
+// const numbers = [22, 55, 12, 1, 8]
+
+// for (let i = 0; i < numbers.length; i++){
+//     // console.log(numbers[i]);
+//     let number = numbers[i];
+//     if (number % 2 !== 0){
+//         console.log(`dispari`);
+//     } else {
+//         console.log(`pari`)
+//     }
+// }
+
+
+
+
+
+const numbers = [12, 34, 87, 54, 65];
+
+for(let i = 0; i < numbers.length; i++) {
+    
+    let number = numbers[i];
+    if(number % 2 === 0) {
+        console.log(number)
+    }
+
+}
